adbd6f17231290d491c729e159ed9bbc36099155
Dariusz Okonski <darek200180@gmail.com>
2020-04-27 16:30:50 +0100

    add connection to mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const indexRoutes = require('./routes/index');
+
+// connection to mongoDB =====================
+mongoose.connect('mongodb://localhost/post_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if(err) {
+        console.log('ERROR: no mongoDB connection...');
+    } else {
+        console.log('MongoDB connected...');
+    }
+});
+
 
 // middlewares ================================
 app.use(express.static('public'));
