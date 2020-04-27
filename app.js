68113fb9a838cec5d5585dbe660744e148a59c6c
Dariusz Okonski <darek200180@gmail.com>
2020-04-27 15:24:45 +0100

    basic start app code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('I am working...');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('I am working...');
+})
+
+
+
+// listener ==============================
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening at port ${port}...`);
+})
