802b98acb8c9e1f0e626a659c50583cccac06c56
Dariusz Okonski <darek200180@gmail.com>
2020-04-27 15:43:56 +0100

    add middleware app.use(express.static(publid))

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const indexRoutes = require('./routes/index');
 
 // middlewares ================================
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.use(indexRoutes);
