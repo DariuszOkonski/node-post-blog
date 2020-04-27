2f80de3c36aa533e3ab84da2deab1f00415ac423
Dariusz Okonski <darek200180@gmail.com>
2020-04-27 16:29:14 +0100

    add route get/posts

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const Post = require('../model/post');
 
 router.get('/', (req, res) => {
     res.redirect('/posts');
 });
 
 router.get('/posts', (req, res) => {
-    res.render('index');
+    Post.find({}, (err, foundPosts) => {
+        if(err) {
+            console.log('ERROR at get/post...');
+        } else {
+            res.render('index', {
+                posts: foundPosts
+            });
+        }
+    });
 })
 
 module.exports = router;
