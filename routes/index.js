0f46a36d16ab3ca65a05d50f8faaea5d83057cb8
Dariusz Okonski <darek200180@gmail.com>
2020-04-28 18:45:22 +0100

    add 2 edit routes, get/posts/id/edit and put/posts/id

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,34 @@ router.get('/posts/:id', (req, res) => {
             });
         }
     });
-})
+});
+
+router.get('/posts/:id/edit', (req, res) => {
+    Post.findById(req.params.id, (err, foundPost) => {
+        if(err) {
+            console.log('ERROR ag get/posts/id/edit...');
+            res.redirect('/');
+        } else {
+            res.render('edit', {
+                post: foundPost
+            });
+        }
+    });
+});
+
+router.put('/posts/:id', (req, res) => {
+    const title = req.sanitize(req.body.post.title);
+    const image = req.sanitize(req.body.post.image);
+    const content = req.sanitize(req.body.post.content);
+
+    Post.findByIdAndUpdate(req.params.id, {title, image, content}, (err) => {
+        if(err) {
+            console.log('ERROR: put/posts/id...');
+            res.redirect('/');
+        } else {
+            res.redirect(`/posts/${req.params.id}`);
+        }
+    })
+});
 
 module.exports = router;
